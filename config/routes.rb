d12acbaa447d2d8bd35455396f55eea1fa12c23d
leopolicastro <lpolicastro@pm.me>
2020-02-23 23:15:52 -0500

    add devise user with admin column

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
