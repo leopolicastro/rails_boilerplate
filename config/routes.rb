db8fbcb78c63d03844db5376ac53326ff31e5db9
leopolicastro <lpolicastro@pm.me>
2020-02-23 23:00:02 -0500

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'welcome/index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'welcome#index'
 end
