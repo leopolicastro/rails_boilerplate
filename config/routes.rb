6af1d36f2e38bfff584ec5c284810f7d0e5798a2
leopolicastro <lpolicastro@pm.me>
2020-02-23 23:42:34 -0500

    Add administrate gem and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user, ->(u) { u.admin? } do
+    namespace :admin do
+      resources :users
+
+      root to: 'users#index'
+    end
+  end
+
   get 'welcome/index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  authenticated :user, ->(u) { u.admin? } do
-  end
 
   root to: 'welcome#index'
 end
