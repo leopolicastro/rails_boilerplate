0a96b3bf00114ab2c579fbfdad577e8755333b4d
leopolicastro <lpolicastro@pm.me>
2020-02-23 23:24:59 -0500

    Add bootstrap navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  authenticated :user, ->(u) { u.admin? } do
+  end
+
   root to: 'welcome#index'
 end
