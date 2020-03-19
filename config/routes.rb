73e2ca680dcf52772f1da060d6c131544c885340
leopolicastro <lpolicastro@pm.me>
2020-03-19 11:21:37 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :admin do
+  authenticated :user, ->(u) { u.admin? } do
+    namespace :admin do
       resources :users
 
-      root to: "users#index"
+      root to: 'users#index'
     end
+  end
+
+  authenticated :user do
+  end
+
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
