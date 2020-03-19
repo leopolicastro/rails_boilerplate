5866113c2ebbc59016aa0f2ac9b4bc1597d2b968
leopolicastro <lpolicastro@pm.me>
2020-03-19 11:16:42 -0400

    configure admin users with administrate gem and pull in admin views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
