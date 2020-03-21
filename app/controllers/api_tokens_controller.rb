ba5347cbdc4e3e22dcad81c0545f84dc408fbb4b
leopolicastro <lpolicastro@pm.me>
2020-03-21 00:22:55 -0400

    api auth enabled

---
+++ app/controllers/api_tokens_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ApiTokensController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    current_user.update(authentication_token: nil)
+    redirect_to edit_user_registration_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -46,14 +46,6 @@ ActiveRecord::Schema.define(version: 2020_03_21_033808) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "announcements", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "category"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
